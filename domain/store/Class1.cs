46721f6698551ac74046448899f4004f1a6af3d3
trololoproorig <125358268+trololoproorig@users.noreply.github.com>
2025-05-15 13:08:48 +0300

    создание проекта

---
+++ domain/store/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace Store
+{
+    public class Shop
+    {
+        public int Id { get; }
+
+        public string Title { get; }
+
+        public Shop(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+
+    }
+}
